a93187c6dc8291ee11cb6515488d5ef610c9c3ee
Sonia Reilly <sonia.reilly@nyu.edu>
2022-04-23 13:34:54 -0400

    added prolongation and restriction

--- multigrid.cpp
+++ multigrid.cpp
@@ -3,6 +3,32 @@
 //#include "prolres.h"  // Prolongation + restriction header
 //#include "gs.h"       // Gauss-Seidel header
 
+// u is row major
+
+void prolongation(double* up, double* u, int n){
+    // up is the output, size 2n+1 x 2n+1, u is the input, size n+1 x n+1
+    for (int i = 0; i < n+1; i++){
+        for (int j = 0; j < n+1; j++){
+            up[2*i*(2*n+1) + 2*j] = u[i*(n+1) + j];
+            up[(2*i+1)*(2*n+1) + 2*j] = (u[i*(n+1) + j] + u[(i+1)*(n+1) + j])/2;
+            up[2*i*(2*n+1) + 2*j+1] = (u[i*(n+1) + j] + u[i*(n+1) + j+1])/2;
+            up[(2*i+1)*(2*n+1) + 2*j+1] = (u[i*(n+1) + j] + u[(i+1)*(n+1) + j] + u[i*(n+1) + j+1] + u[(i+1)*(n+1) + j+1])/4;
+        }
+    }
+}
+
+void restriction(double* u, double* up, int n){
+    // u is the output, size n/2+1 x n/2+1, up is the input, size n+1 x n+1
+    for (int i = 0; i < n/2+1; i++){
+        for (int j = 0; j < n/2+1; j++){
+            u[i*(n/2+1)+j] = (up[(2*i-1)*(n+1)+2*j-1] + 2*up[(2*i-1)*(n+1)+2*j] + up[(2*i-1)*(n+1)+2*j+1])/16;
+            u[i*(n/2+1)+j] += (2*up[2*i*(n+1)+2*j-1] + 4*up[2*i*(n+1)+2*j] + 2*up[2*i*(n+1)+2*j+1])/16;
+            u[i*(n/2+1)+j] += (up[(2*i+1)*(n+1)+2*j-1] + 2*up[(2*i+1)*(n+1)+2*j] + up[(2*i+1)*(n+1)+2*j+1])/16;
+
+        }
+    }
+}
+
 void mg_inner(double* up, double* u, 
               double** f, double* r, 
               double dx, int n, 
