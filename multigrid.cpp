84f4b0bf734045e2093ee00978e8b876ac46272c
Sonia Reilly <sonia.reilly@nyu.edu>
2022-04-24 15:18:18 -0400

    bugfix: r not defined

--- multigrid.cpp
+++ multigrid.cpp
@@ -61,7 +61,7 @@ nu                       - diffusion parameter
         // I don't know how many to use; experiment!
     
     // compute coefficient for LHS and RHS
-    double rr = r(dx,dt);
+    double rr = 1.0/2*dt/(dx*dx); //r(dx,dt); but r isn't defined in this scope
 
     double *ui = u[lvl];
     double *rhsi = rhs[lvl];
