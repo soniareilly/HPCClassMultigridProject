2b76f1234b30a1373835665374e69d1bb398a7fe
Sonia Reilly <sonia.reilly@nyu.edu>
2022-04-27 22:59:52 -0400

    fixed gs_tester -- we have advection!

--- gs_tester.m
+++ gs_tester.m
@@ -1,27 +1,40 @@
-n = 6;
+clear all; close all;
+
+n = 50;
+dt = 0.005;
+dx = 1/n;
 v1 = zeros(n+1,n+1);
 v2 = zeros(n+1,n+1);
+nu = 0; %-4e-4;
+
 kx = pi;
 ky = pi;
+
+x0 = 0.2; y0 = 0.4;
+sigma = 100.0;
+u = zeros(n+1,n+1);
+%u(5,5) = 100;
 for i = 1:n+1
     for j = 1:n+1
         v1(i,j) = -ky*sin(kx*(i-1)*dx)*cos(ky*(j-1)*dx);
         v2(i,j) = kx*cos(kx*(i-1)*dx)*sin(ky*(j-1)*dx);
+        u(i,j) = exp(-sigma*(((i-1)*dx-x0)*((i-1)*dx-x0) + ((j-1)*dx-y0)*((j-1)*dx-y0)));
     end
 end
-v1
-v2
-nu = 0; %-4e-4;
-dt = 0.005;
-dx = 1/n;
 
-u = zeros(n+1,n+1);
-u(3,3) = 100;
-disp(u)
-for i=1:3
+for i = 1:100
     rhs = compute_rhs(u, n, v1, v2, dt, nu, dx);
-    u = gauss_seidel(u, rhs, n, v1, v2, dt, nu, dx)
+    resnorm = 1;
+    while resnorm > 1e-5
+        u = gauss_seidel(u, rhs, n, v1, v2, dt, nu, dx);
+        res = residual(u, rhs, n, v1, v2, dt, nu, dx);
+        resnorm = compute_norm(res, n);
+    end
+    pcolor(u)
+    pause(0.05)
 end
+
+
 
 function rr = r(h,k)
     rr = 1.0/2*k/(h*h);
@@ -95,7 +108,7 @@ function u = gauss_seidel(u, rhs, n, v1, v2, k, nu, h)
             bb = b(v2(i,j),nu,h,rr);
             cc = a(v1(i,j),nu,h,rr);
             dd = b(v1(i,j),nu,h,rr);
-            u(i,j) = (rhs(i,j) - cc*u(i-1,j) - dd*u(i+1,j) - aa*u(i,j-1) - bb*u(i,j+1))/(1.0-4.0*rr*nu);
+            u(i,j) = (rhs(i,j) - cc*u(i-1,j) - aa*u(i,j-1) - dd*u(i+1,j) - bb*u(i,j+1))/(1.0-4.0*rr*nu);
             j = j+2;
         end
     end
@@ -109,7 +122,7 @@ function u = gauss_seidel(u, rhs, n, v1, v2, k, nu, h)
             bb = b(v2(i,j),nu,h,rr);
             cc = a(v1(i,j),nu,h,rr);
             dd = b(v1(i,j),nu,h,rr);
-            u(i,j) = (rhs(i,j) - cc*u(i-1,j) - dd*u(i+1,j) - aa*u(i,j-1) - bb*u(i,j+1))/(1.0-4.0*rr*nu);
+            u(i,j) = (rhs(i,j) - cc*u(i-1,j) - aa*u(i,j-1) - dd*u(i+1,j) - bb*u(i,j+1))/(1.0-4.0*rr*nu);
             j = j+2;
         end
     end
