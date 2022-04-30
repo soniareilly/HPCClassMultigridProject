93c993ee99eb5b1b810baf2e466998a106f393cb
Tanya <yw3087@nyu.edu>
2022-04-29 21:49:30 -0400

    debug prolrestest omp

--- prolrestest.cpp
+++ prolrestest.cpp
@@ -10,21 +10,28 @@ void prolongation(double* up, double* u, int n){
     // up is the output, size 2n+1 x 2n+1, u is the input, size n+1 x n+1
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
+            #pragma omp task
+            {
             up[2*i*(2*n+1) + 2*j] = u[i*(n+1) + j];
             up[(2*i+1)*(2*n+1) + 2*j] = (u[i*(n+1) + j] + u[(i+1)*(n+1) + j])/2;
             up[2*i*(2*n+1) + 2*j+1] = (u[i*(n+1) + j] + u[i*(n+1) + j+1])/2;
             up[(2*i+1)*(2*n+1) + 2*j+1] = (u[i*(n+1) + j] + u[(i+1)*(n+1) + j] + u[i*(n+1) + j+1] + u[(i+1)*(n+1) + j+1])/4;
+            }
         }
     }
     // right and bottom borders
     for (int i = 0; i < n; i++){
         // right border
+        #pragma omp task
+        {
         up[2*i*(2*n+1) + 2*n] = u[i*(n+1) + n];
         up[(2*i+1)*(2*n+1) + 2*n] = (u[i*(n+1) + n] + u[(i+1)*(n+1) + n])/2;
         // bottom border
         up[2*n*(2*n+1) + 2*i] = u[n*(n+1) + i];
         up[2*n*(2*n+1) + 2*i+1] = (u[n*(n+1) + i] + u[n*(n+1) + i+1])/2;
+        }
     }
+    #pragma omp taskwait
     // bottom right corner
     up[(2*n)*(2*n+1) + 2*n] = u[n*(n+1) + n];
 }
@@ -36,9 +43,11 @@ void restriction(double* u, double* up, int n){
             //u[i*(n/2+1)+j] = (up[(2*i-1)*(n+1)+2*j-1] + 2*up[(2*i-1)*(n+1)+2*j] + up[(2*i-1)*(n+1)+2*j+1])/16;
             //u[i*(n/2+1)+j] += (2*up[2*i*(n+1)+2*j-1] + 4*up[2*i*(n+1)+2*j] + 2*up[2*i*(n+1)+2*j+1])/16;
             //u[i*(n/2+1)+j] += (up[(2*i+1)*(n+1)+2*j-1] + 2*up[(2*i+1)*(n+1)+2*j] + up[(2*i+1)*(n+1)+2*j+1])/16;
+            #pragma omp task
             u[i*(n/2+1)+j] = up[2*i*(n+1)+2*j];
         }
     }
+    #pragma omp taskwait
 }
 
 int main(){
@@ -49,58 +58,78 @@ int main(){
     u = (double*) malloc ( sizeof(double) * (2*N+1)*(2*N+1) );
     up2 = (double*) malloc ( sizeof(double) * (N+1)*(N+1) );
 
-    #pragma omp parallel num_threads(16)
+    uomp = (double*) malloc ( sizeof(double) * (2*N+1)*(2*N+1) );
+    up2omp = (double*) malloc ( sizeof(double) * (N+1)*(N+1) );
+
+    // Initialize up
+    for (int i = 0; i < N+1; i++){
+        for(int j = 0; j < N+1; j++){
+
+            up[i*(N+1)+j] = i+j;
+
+        }
+    }
+
+    double tt = omp_get_wtime();
+    prolongation(u, up, N);
+    // restriction(up2, u, 2*N);
+    printf("\nCPU (1 thread) time: %f s\n", omp_get_wtime()-tt);
+
+    int ntr = 16;
+    tt = omp_get_wtime();
+    #pragma omp parallel num_threads(ntr)
     {
     
-    #pragma omp single
+    #pragma omp single // manager thread
     {
     int tid = omp_get_thread_num();
     if(tid == 0) printf("number of threads: %d\n", omp_get_num_threads());
-    // Initialize up
-    //#pragma omp for
-    for (int i = 0; i < N+1; i++){
-        for(int j = 0; j < N+1; j++){
-            #pragma omp task
-{           
-            tid = omp_get_thread_num();
-            printf("thread id: %d\n", tid);
-            up[i*(N+1)+j] = i+j;
-}
+    
+    prolongation(uomp, up, N);
+    // restriction(up2omp, u, 2*N);
+    
+    }
+    }
+    printf("\nCPU with OMP (%d threads) time: %f s\n", ntr, omp_get_wtime()-tt);
+    // compute error wrt the referenced solution
+    for (i = 0; i < N+1; i++){
+        for (j = 0; j < N+1; j++){
+            error += fabs(u[i*(N+1)+j]-uomp[i*(N+1)+j]);
+            // error += fabs(up2[i*(N+1)+j]-up2omp[i*(N+1)+j]);
         }
     }
-    #pragma omp taskwait
+    printf("Error = %10e\n", error);
 
-    printf("Original matrix\n");
-    for (int i = 0; i < N+1; ++i)
-    {   
-        for (int j = 0; j < N+1; ++j)
-        {
-            printf("%.1f\t",up[i*(N+1)+j]);
-        }
-        printf("\n");
-    }
-    prolongation(u, up, N);
-    printf("Prolongated matrix\n");
-    for (int i = 0; i < 2*N+1; ++i)
-    {   
-        for (int j = 0; j < 2*N+1; ++j)
-        {
-            printf("%.1f\t",u[i*(2*N+1)+j]);
-        }
-        printf("\n");
-    }
-    restriction(up2, u, 2*N);
-    printf("Restriction matrix\n");
-    for (int i = 0; i < N+1; ++i)
-    {   
-        for (int j = 0; j < N+1; ++j)
-        {
-            printf("%.1f\t",up2[i*(N+1)+j]);
-        }
-        printf("\n");
-    }
-    }
-    }
+
+    // printf("Original matrix\n");
+    // for (int i = 0; i < N+1; ++i)
+    // {   
+    //     for (int j = 0; j < N+1; ++j)
+    //     {
+    //         printf("%.1f\t",up[i*(N+1)+j]);
+    //     }
+    //     printf("\n");
+    // }
+    // prolongation(u, up, N);
+    // printf("Prolongated matrix\n");
+    // for (int i = 0; i < 2*N+1; ++i)
+    // {   
+    //     for (int j = 0; j < 2*N+1; ++j)
+    //     {
+    //         printf("%.1f\t",u[i*(2*N+1)+j]);
+    //     }
+    //     printf("\n");
+    // }
+    // restriction(up2, u, 2*N);
+    // printf("Restriction matrix\n");
+    // for (int i = 0; i < N+1; ++i)
+    // {   
+    //     for (int j = 0; j < N+1; ++j)
+    //     {
+    //         printf("%.1f\t",up2[i*(N+1)+j]);
+    //     }
+    //     printf("\n");
+    // }
 
     free(up);
     free(u);
